45af93297379f27a20242197e31d08af7982784d
Domenik Töfflinger <domenikt96@yahoo.de>
2022-04-12 18:48:48 +0200

    locations

---
+++ src/interfaces/Location.ts
@@ -0,0 +1,11 @@
+export interface Location {
+  id: number
+  name: string
+  email: string
+  replyTo: string
+  description: string
+  location: string
+  timezone: string
+  thumbnail: boolean
+  image: boolean
+}
--- src/views/AppointmentCreateView.tsx
+++ src/views/AppointmentCreateView.tsx
@@ -1,37 +1,74 @@
-import React from 'react'
-import { withAuthenticationRequired } from '@auth0/auth0-react'
+import React, { useEffect, useState } from 'react'
+import { useAuth0, withAuthenticationRequired } from '@auth0/auth0-react'
 import Loading from '../components/Loading'
-import { Stack, Divider, Text, HStack, Tooltip } from '@healform/liquid'
+import { Stack, Divider, Text, HStack, Body, useToast } from '@healform/liquid'
 import { PageHeader } from '../components/PageHeader'
 import { RadioCard, RadioCardGroup } from '../components/RadioCardGroup'
-import { IconButton } from '@chakra-ui/react'
-import { FiRefreshCw, FiShoppingCart } from 'react-icons/fi'
-
-const locations = [
-  {
-    id: 1,
-    name: 'Kassel',
-    description: '',
-    address: '',
-    acuityId: '',
-  },
-  {
-    id: 2,
-    name: 'Baunatal',
-    description: '',
-    address: '',
-    acuityId: '',
-  },
-  {
-    id: 3,
-    name: 'Warburg',
-    description: '',
-    address: '',
-    acuityId: '',
-  },
-]
+import { Location } from '../interfaces/Location'
 
 const AppointmentCreateView: React.FC = () => {
+  const [locations, setLocations] = useState<Location[]>([])
+  const [dates, setDates] = useState()
+  const [isLoading, setLoading] = useState(true)
+  const [isError, setError] = useState(false)
+  const toast = useToast()
+
+  useEffect(() => {
+    const getLocations = () => {
+      fetch(`${process.env.REACT_APP_API_URL}` + `/acuity/locations`)
+        .then(response => response.json())
+        .then(json => {
+          /* Set certificates */
+          setLocations(json)
+          /* Disable loading */
+          setLoading(false)
+        })
+        .catch(err => {
+          if (err.response) {
+            setLoading(false)
+            setError(true)
+            const id = 'errorResponse'
+            if (!toast.isActive(id)) {
+              toast({
+                title: 'Die Daten konnten nicht geladen werden.',
+                description: 'Der Server gab eine Fehlermeldung zurück.',
+                status: 'error',
+                isClosable: true,
+                position: 'bottom-right',
+                id,
+              })
+            }
+          } else if (err.request) {
+            setLoading(false)
+            setError(true)
+            const id = 'errorRequest'
+            if (!toast.isActive(id)) {
+              toast({
+                title: 'Die Daten konnten nicht geladen werden.',
+                description: 'Möglicherweise besteht keine Verbinung zum Internet oder Server.',
+                status: 'error',
+                isClosable: true,
+                position: 'bottom-right',
+                id,
+              })
+            }
+          }
+        })
+    }
+
+    getLocations()
+  }, [toast])
+
+  const getAvailableDates = (locationId: string) => {
+    const appointmentId = '5780353'
+    fetch(`${process.env.REACT_APP_API_URL}` + `/acuity/availability/dates/` + appointmentId + `/2022-04/` + locationId)
+      .then(response => response.json())
+      .then(json => {
+        setDates(json)
+        console.log(json)
+      })
+  }
+
   return (
     <>
       <Stack spacing={5}>
@@ -42,23 +79,23 @@ const AppointmentCreateView: React.FC = () => {
             justify="space-between"
             align={{ base: 'start', md: 'center' }}
           >
-            <PageHeader title={'Neuer Termin'} subtitle={'Tell others who you are'} />
-            <HStack spacing="3"></HStack>
+            <PageHeader title={'Neuer Termin'} subtitle={''} />
           </Stack>
           <Divider />
-          <RadioCardGroup defaultValue="one" spacing="3">
+          <RadioCardGroup defaultValue="one" spacing="3" onChange={e => getAvailableDates(e)}>
             {locations.map(location => (
-              <RadioCard key={location.id} value={location.acuityId}>
-                <Text color="primary.500" fontWeight="medium" fontSize="sm">
+              <RadioCard key={location.id} value={location.id.toString()}>
+                <Body noMargin variant="highlight" color="primary.500">
                   {location.name}
-                </Text>
-                <Text color="gray.600" fontSize="sm">
-                  Jelly biscuit muffin icing dessert powder macaroon.
-                </Text>
+                </Body>
+                <Body noMargin color="gray.600">
+                  {location.location}
+                </Body>
               </RadioCard>
             ))}
           </RadioCardGroup>
         </Stack>
+        <PageHeader title={'Datum auswählen'} subtitle={''} />
       </Stack>
     </>
   )
